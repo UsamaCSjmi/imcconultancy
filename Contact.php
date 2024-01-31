174c3550ad8be8b0d264cd115ae3cdf765e909a4
danish-iliyas <dshekh850@gmail.com>
2024-01-31 17:09:01 +0530

    Contact  section completed

--- Contact.php
+++ Contact.php
@@ -2,69 +2,80 @@
 include_once("assets/header.php");
 ?>
 <div class="Contact_containerbody">
-   
 
-<div class="contactimg">
-      <img src="contact_img/Contact-US.jpg" class="card-img-top" alt="...">
-      <h1> Contact Us </h1>
+
+  <div class="contactimg">
+    <img src="contact_img/Contact-US.jpg" class="card-img-top" alt="...">
+    <h1> Contact Us </h1>
+  </div>
+
+
+
+  <div class="contactdetail1">
+    <div class="contactdetail">
+      <div class="icon"></div>
+      <div class="detail">
+        <h3>Address</h3>
+        <p> ahaad khan iliyas ahamad what Lorem ahaad khan iliyas ahamad what Lorem ilyas ahaad khan iliyas ahamad
+          what Lorem, ipsum dolor. Lorem ipsum, .</p>
+      </div>
     </div>
-  <div class="contact_container">
-    
-    <div class="contactdetail1">
-      <div class="contactdetail">
-        <div class="icon"></div>
-        <div class="detail">
-          <h3>Address</h3>
-          <p> ahaad khan iliyas ahamad what Lorem ahaad khan iliyas ahamad what Lorem ilyas ahaad khan iliyas ahamad
-            what Lorem, ipsum dolor. Lorem ipsum, .</p>
-        </div>
+    <div class="contactdetail">
+      <div class="icon"></div>
+      <div class="detail">
+        <h3>Phone</h3>
+        <p> ilyas ahaad khan iliyas ahamad what dolor sit amet consectetur adipisicing elit. Tenetur</p>
       </div>
-      <div class="contactdetail">
-        <div class="icon"></div>
-        <div class="detail">
-          <h3>Phone</h3>
-          <p> ilyas ahaad khan iliyas ahamad what dolor sit amet consectetur adipisicing elit. Tenetur</p>
-        </div>
-      </div>
-      <div class="contactdetail">
-        <div class="icon"></div>
-        <div class="detail">
-          <h3>Web</h3>
-          <p> dolor sit amet consectetur adipisicing elit. Tenetur ilyas ahaad khan iliyas ahamad what</p>
-        </div>
+    </div>
+    <div class="contactdetail">
+      <div class="icon"></div>
+      <div class="detail">
+        <h3>Web</h3>
+        <p> dolor sit amet consectetur adipisicing elit. Tenetur ilyas ahaad khan iliyas ahamad what</p>
       </div>
     </div>
   </div>
-  <h3>Leave a message here</h3>
+
   <div class="contact_form">
-  
-<form action="">
-              <!-- <p>--</p> -->
-              <div ><input type="text" placeholder="name"></div>
-              <div><input type="number" placeholder ="phone"><br></div>
-              <!-- <div><input type="number" placeholder ="phone"></div> -->
-              <div><input type="email" placeholder ="email"></div>
-              <div><input type="number" placeholder ="Neet Score"></div>
-              <div><input type="text" placeholder ="city"></div>
-               <div><input type="text" placeholder ="State"><br></div>
-               <div><textarea name="" id="textarea" cols="30" rows="10" placeholder ="Write here ..."></textarea><br></div>
-                      <button>Submit</button>
-</form>
+    <h3>Leave a message here</h3>
 
-          
-  </div>
-  
-
-
-
-
-
-
-
-
+    <form action="">
+      <!-- <p>--</p> -->
+      <div><input type="text" placeholder="name"></div>
+      <div><input type="number" placeholder="phone"><br></div>
+      <!-- <div><input type="number" placeholder ="phone"></div> -->
+      <div><input type="email" placeholder="email"></div>
+      <div><input type="number" placeholder="Neet Score"></div>
+      <div><input type="text" placeholder="city"></div>
+      <div><input type="text" placeholder="State"><br></div>
+      <div><textarea name="" id="textarea" cols="30" rows="10" placeholder="Write here ..."></textarea><br></div>
+      <div class="button"> <button>Submit</button></div>
+    </form>
 
 
   </div>
+
+  <div class="subscribe_section">
+    <div class="subscribe_detail">
+          <div class="subscribe_left"><h1>Subscribe our <br> Nesletter</h1><br> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati fuga enim autem harum, velit .</p> </div>
+          <div class="subscribe_right"><input type="text" placeholder = "Enter email here">
+            <button>Subscribe now</button>
+        </div>
+
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
 
 <?php
 include_once("assets/footer.php");
--- assets/head.php
+++ assets/head.php
@@ -32,6 +32,8 @@
     <!-- -------------Bootstrap------------------- -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- link for cdn -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <!-- Magnific Popup core CSS file -->
     <link rel="stylesheet" href="magnific-popup/magnific-popup.css">
@@ -57,7 +59,7 @@
     <link rel="stylesheet" href="<?php echo SITE_PATH;?>/css/About_responsive.css?v=<?php echo time()?>" />
     <link rel="stylesheet" href="<?php echo SITE_PATH;?>/css/Contact.css?v=<?php echo time()?>" />
     <link rel="stylesheet" href="<?php echo SITE_PATH;?>/css/Contact_responsive.css?v=<?php echo time()?>" />
-
+      
     <link rel="stylesheet" href="<?php echo SITE_PATH;?>/css/Header.css" />
     <link rel="stylesheet" href="<?php echo SITE_PATH;?>/css/Footer.css" />
   </head>
